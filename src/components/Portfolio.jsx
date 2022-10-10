4e1484c3503284370535cd183b301c95cfc4296d
LeithPrice <leith.price@gmail.com>
2022-10-10 14:01:39 +1000

    made portfolio page responsivev2

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,95 +2,78 @@ import React from "react";
 
 const Portfolio = () => {
   return (
-    // <div class="container text-center text-md-start mt-5">
-    //   <div class="row mt-3">
-    //     <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-
-    //     <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-
-    //     <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-
-    //     <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-
-    //     </div>
-    //   </div>
-    // </div>
-
-    // <div class="d-flex flex-wrap justify-content-center border-bottom " >
-
-
     <section class="d-flex flex-wrap justify-content-center text-center  mt-5">
       <div class="row">
-      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-        <h1>Blog One</h1>
-        <h3>Will Dogs Ever Rule The World?</h3>
-        <div class="image">
-          <img src="/images/Dog.jfif" alt="one"></img>
+        <div class="col-md-4 col-lg-3 col-xl-2 mx-auto mb-4">
+          <h1>Blog One</h1>
+          <h3>Will Dogs Ever Rule The World?</h3>
+          <div class="image">
+            <img src="/images/Dog.jfif" alt="one"></img>
+          </div>
+          <p>Dec 7, 2017</p>
+          <p>
+            Sunt in culpa qui officia deserunt mollit anim id est laborum
+            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco.
+          </p>
         </div>
-        <p>Dec 7, 2017</p>
-        <p>
-          Sunt in culpa qui officia deserunt mollit anim id est laborum
-          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-          exercitation ullamco.
-        </p>
-      </div>
-      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-        <h1>Blog Two</h1>
-        <h3>Barbwire: Expectations vs. Reality</h3>
-        <div class="image">
-          <img src="/images/barb_wire.jpg" alt="one"></img>
+        <div class="col-md-4 col-lg-3 col-xl-2 mx-auto mb-4">
+          <h1>Blog Two</h1>
+          <h3>Barbwire: Expectations vs. Reality</h3>
+          <div class="image">
+            <img src="/images/barb_wire.jpg" alt="one"></img>
+          </div>
+          <p>Sep 2, 2017</p>
+          <p>
+            Sunt in culpa qui officia deserunt mollit anim id est laborum
+            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco.
+          </p>
         </div>
-        <p>Sep 2, 2017</p>
-        <p>
-          Sunt in culpa qui officia deserunt mollit anim id est laborum
-          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-          exercitation ullamco.
-        </p>
-      </div>
-      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-        <h1>Blog Three</h1>
-        <h3>The Next Big Thing in Carnival Rides</h3>
-        <div class="image">
-          <img src="/images/carnivalride.jfif" alt="one"></img>
+        <div class="col-md-4 col-lg-3 col-xl-2 mx-auto mb-4">
+          <h1>Blog Three</h1>
+          <h3>The Next Big Thing in Carnival Rides</h3>
+          <div class="image">
+            <img src="/images/carnivalride.jfif" alt="one"></img>
+          </div>
+          <p>July 9, 2017</p>
+          <p>
+            Sunt in culpa qui officia deserunt mollit anim id est laborum
+            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco.
+          </p>
         </div>
-        <p>July 9, 2017</p>
-        <p>
-          Sunt in culpa qui officia deserunt mollit anim id est laborum
-          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-          exercitation ullamco.
-        </p>
-      </div>
-      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-        <h1>Blog Four</h1>
-        <h3>20 Myths about Fish: Busted</h3>
-        <div class="image">
-          <img src="/images/FISH.jfif" alt="one"></img>
+        <div class="col-md-4 col-lg-3 col-xl-2 mx-auto mb-4">
+          <h1>Blog Four</h1>
+          <h3>20 Myths about Fish: Busted</h3>
+          <div class="image">
+            <img src="/images/FISH.jfif" alt="one"></img>
+          </div>
+          <p>Apr 13, 2017</p>
+          <p>
+            Sunt in culpa qui officia deserunt mollit anim id est laborum
+            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco.
+          </p>
         </div>
-        <p>Apr 13, 2017</p>
-        <p>
-          Sunt in culpa qui officia deserunt mollit anim id est laborum
-          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-          exercitation ullamco.
-        </p>
-      </div>
-      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-        <h1>Blog Five</h1>
-        <h3>The Top Reasons People Succeed in the Ambulance Industry</h3>
-        <div class="image">
-          <img src="/images/ambulance2.jfif" alt="one"></img>
+        <div class="col-md-4 col-lg-3 col-xl-2 mx-auto mb-4">
+          <h1>Blog Five</h1>
+          <h3>The Top Reasons People Succeed in the Ambulance Industry</h3>
+          <div class="image">
+            <img src="/images/ambulance2.jfif" alt="one"></img>
+          </div>
+          <p>Jan 14, 2017</p>
+          <p>
+            Sunt in culpa qui officia deserunt mollit anim id est laborum
+            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco.
+          </p>
         </div>
-        <p>Jan 14, 2017</p>
-        <p>
-          Sunt in culpa qui officia deserunt mollit anim id est laborum
-          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-          exercitation ullamco.
-        </p>
-      </div>
       </div>
     </section>
   );
